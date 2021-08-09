2c5c1245e4b0e59bf9bce3f769141a6e0bc23676
marvin-ramos <ramosmarvin50@gmail.com>
2021-08-09 22:51:27 +0800

    Creating Our Header navigation

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -1,4 +1,4 @@
-import styled from 'style-components';
+import styled from 'styled-components';
 import { Link as LinkRouter } from 'react-router-dom';
 import { Link as LinkScroll } from 'react-scroll';
 
@@ -39,3 +39,48 @@ export const NavLogo = styled(LinkRouter)`
 	font-weight: bold;
 	text-decoration: none;
 ` 
+
+export const MobileIcon = styled.div`
+	display: none;
+
+	@media screen and (max-width: 768px) {
+		display: block;
+		position: absolute;
+		top: 0;
+		right: 0;
+		transform: translate(-100%, 60%);
+		font-size: 1.8rem;
+		cursor: pointer;
+		color: #fff;
+	}
+`
+
+export const NavMenu = styled.ul`
+	display: flex;
+	align-items: center;
+	list-style: none;
+	text-align: center;
+	margin-right: -22px;
+
+	@media screen and (max-width: 768px) {
+		display: none;
+	}
+`
+
+export const NavItem = styled.li`
+	height: 80px;
+`
+
+export const NavLinks = styled(LinkScroll)`
+	color: #fff;
+	display: flex;
+	align-items: center;
+	text-decoration: none;
+	padding: 0 1rem;
+	height: 100%;
+	cursor: pointer;
+
+	&.active {
+		border-bottom: 3px solid #01bf71;
+	}
+`
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,5 +1,16 @@
 import React from 'react';
-import { Nav, NavbarContainer, NavLogo } from './NavbarElements';
+import {FaBars} from 'react-icons/fa';
+import { HiUserCircle } from "react-icons/hi";
+
+import { 
+	Nav, 
+	NavbarContainer, 
+	NavLogo, 
+	MobileIcon, 
+	NavMenu, 
+	NavItem, 
+	NavLinks,
+	} from './NavbarElements';
 
 const index = () => {
 	return (
@@ -7,6 +18,34 @@ const index = () => {
 			<Nav>
 				<NavbarContainer>
 					<NavLogo to="/">MNTN</NavLogo>
+					
+					<MobileIcon>
+						<FaBars />
+					</MobileIcon>
+
+					<NavMenu>
+						<NavItem>
+							<NavLinks to='about'>About us</NavLinks>
+						</NavItem>
+						<NavItem>
+							<NavLinks to='project'>Projects</NavLinks>
+						</NavItem>
+						<NavItem>
+							<NavLinks to='service'>Services</NavLinks>
+						</NavItem>
+						<NavItem>
+							<NavLinks to='join'>Join us</NavLinks>
+						</NavItem>
+						<NavItem>
+							<NavLinks to='update'>Updates</NavLinks>
+						</NavItem>
+					</NavMenu>
+
+					<NavMenu>
+						<NavItem>
+							<NavLinks to='accoun'><HiUserCircle/> Account</NavLinks>
+						</NavItem>
+					</NavMenu>		
 				</NavbarContainer>
 			</Nav>
 		</>
